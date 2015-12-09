59e8fc53e4147c9fec67a8e0be4ab7f111c9c7d1
Hawk Weisman <hi@hawkweisman.me>
2015-12-09 11:39:40 -0500

    Rewriting force-directed stuff

--- src/FDGraph.hs
+++ src/FDGraph.hs
@@ -1,20 +1,36 @@
-module FDGraph where
+module FDGraph.EdgeWeighted where
 
 -----------------------------------------------------------------------------
 import Linear.V2
 import Linear.Vector
 -----------------------------------------------------------------------------
 
-type Vec = V2 Integer
-type AttractVec = (Vec, Integer)
+-- type Vec = V2 Integer
+-- type AttractVec = (Vec, Integer)
+--
+-- data WordNode
+--     = WordNode { word  :: !String
+--                , count :: !Integer
+--                , pos   :: !Vec
+--                , force :: !Vec
+--                }
+--
+-- sumForces :: Vec -> [AttractVec] -> Vec
+-- sumForces = foldr ((^+^) . applyForce)
+--     where applyForce (vec, force) = vec ^ force
+--
+-- updateNode :: WordNode -> WordNode
+-- updateNode = undefined
 
-data WordNode
-    = WordNode { word  :: !String
-               , count :: !Integer
-               , pos   :: !Vec
-               , attraction :: [AttractVec]
-               }
+data Node = Node { pV :: V2 Integer -- Node position vector
+                 , fV :: V2 Integer -- Node force vector
+                 , edges :: [(Node, Float)]
+                 } deriving (Show, Read, Eq)
 
-sumForces :: Vec -> [AttractVec] -> Vec
-sumForces = foldr ((^+^) . applyForce)
-    where applyForce (vec, force) = vec ^ force
+
+repulse :: (Fractional n) => n -> n -> n
+repulse k_r d = k_r / (d*d)
+
+
+spring :: (Fractional n) => n -> n -> n -> n
+spring k_s l d = k_s * (d - l)
